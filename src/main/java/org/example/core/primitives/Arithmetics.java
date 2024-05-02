ccbb7a3c1eefb86436d4d77c76e9895c134338e6
Pls <iamnotpls@gmail.com>
2024-05-02 18:35:45 +0300

    math primitives

---
+++ src/main/java/org/example/core/primitives/Arithmetics.java
@@ -0,0 +1,11 @@
+package org.example.core.primitives;
+
+public class Arithmetics {
+    public static void main(String[] args) {
+        System.out.println(plusCalculateInt(1,2));
+    }
+
+    public static int plusCalculateInt(int a, int b) {
+        return a+b;
+    }
+}
