2cf9e2f439dc80cecde55fc4d7a28d4a1520368a
polta <pol.tarelki@mail.ru>
2024-05-03 15:03:50 +0500

    first task

--- src/main/java/org/example/core/primitives/Arithmetics.java
+++ src/main/java/org/example/core/primitives/Arithmetics.java
@@ -10,10 +10,17 @@ package org.example.core.primitives;
  */
 public class Arithmetics {
     public static void main(String[] args) {
+
         System.out.println(plusCalculateInt(1,2));
+
+        System.out.println(minusCalculateInt(1,2));
     }
 
     public static int plusCalculateInt(int a, int b) {
         return a+b;
+    }
+
+    public static int minusCalculateInt(int a, int b) {
+        return a-b;
     }
 }
