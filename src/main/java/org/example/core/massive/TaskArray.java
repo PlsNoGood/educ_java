38024830241a43ea6f48fc4799b46c9e3dd08215
polta <pol.tarelki@mail.ru>
2024-05-20 12:57:05 +0500

    Переделал первую задачу по Массивам

--- src/main/java/org/example/core/massive/TaskArray.java
+++ src/main/java/org/example/core/massive/TaskArray.java
@@ -11,17 +11,11 @@ public class TaskArray {
         //твой код здесь
 
         int[] arr = new int[10];
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-        arr[3] = 4;
-        arr[4] = 5;
-        arr[5] = 6;
-        arr[6] = 7;
-        arr[7] = 8;
-        arr[8] = 9;
-        arr[9] = 10;
-        System.out.println(arr[5]);
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = i+1;
+        }
+        System.out.println(arr[9]);
+
 
         // Найдите максимальный элемент в массиве. (создать любой массив с рандомными числами)
         //твой код здесь
